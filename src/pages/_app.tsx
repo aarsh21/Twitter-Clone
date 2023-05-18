fe24f2b23a424defb50fb44250684a774e3669f9
aarsh21 <aarshpersonal21@gmail.com>
2023-05-18 17:35:12 +0530

    added changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
+import { SideNav } from "~/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +14,18 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Twitter Clone</title>
+        <meta name="description" content="This is twitter clone by aarsh" />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+
+      <div className="container mx-auto flex items-start sm:pr-40">
+        <SideNav />
+        <div className="main border-x min-h-screen flex-grow ">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
